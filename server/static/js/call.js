0be58b6a98b186f37f246d34649c0b819252d44a
Tochukwu Ibe-Ekeocha <tochukwu.claver@gmail.com>
2021-01-25 06:54:27 -0500

    review completed

--- server/static/js/call.js
+++ server/static/js/call.js
@@ -10,20 +10,22 @@ function handleError(error) {
 
 console.log("the session name ", sessionName);
 
-// fetch(`session/${sessionName}/token`)
-//     .then((res) => res.json())
-//     .then(({ token, apiKey, sessionId }) => {
-//         console.log("the data in call ", { token, apiKey, sessionId })
-//         return initializeSession(apiKey, sessionId, token)
-//     })
-//     .catch((err) => {
-//         alert("could not initialize the sessions :(");
-//         console.log("the error ", err);
-//     })
+fetch(`session/${sessionName}/token`)
+    .then((res) => res.json())
+    .then(({ token, apiKey, sessionId }) => {
+        console.log("the data in call ", { token, apiKey, sessionId })
+        return initializeSession(apiKey, sessionId, token)
+    })
+    .catch((err) => {
+        alert("could not initialize the sessions :(");
+        console.log("the error ", err);
+    })
 
 
 function initializeSession(apiKey, sessionId, token) {
     var session = OT.initSession(apiKey, sessionId);
+    let videoOn = true;
+    let audioOn = true
 
     // Subscribe to a newly created stream
     session.on('streamCreated', function (event) {
@@ -50,4 +52,31 @@ function initializeSession(apiKey, sessionId, token) {
             session.publish(publisher, handleError);
         }
     });
+
+
+    document.getElementById("mute").addEventListener('click', ({target}) => {
+        if (audioOn) {
+          
+            audioOn = false;
+            target.innerText = "Unmute"
+        } else {
+            audioOn = true;
+            target.innerText = "mute"
+        }
+
+        publisher.publishAudio(audioOn)
+    })
+
+    document.getElementById("camera").addEventListener("click", ({target}) => {
+        console.log("the camera clicked ", videoOn)
+        if (videoOn) {
+            videoOn = false;
+            target.innerText = "Camera on"
+        } else {
+            videoOn = true;
+            target.innerText = "Camera off"
+        }
+
+        publisher.publishVideo(videoOn)
+    })
 }
